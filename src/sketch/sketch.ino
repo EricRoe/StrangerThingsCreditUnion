d8ca963be8d7763bd12574d2c74890968a89ef51
EricRoe <eric.roe@outlook.com>
2018-12-04 14:46:24 -0800

    Update sketch.ino

--- src/sketch/sketch.ino
+++ src/sketch/sketch.ino
@@ -1,10 +1,10 @@
 #include <FastLED.h>
+
 #include "alphabet.h"
+#include "colors.h"
 
 #define NUM_LEDS 50
 #define DATA_PIN 6
-#define OFF CRGB::Black
-#define JAM 0x982371
 
 CRGB leds[NUM_LEDS];
 int msg[] = {S,T,C,U};
@@ -12,7 +12,6 @@ int msg_length = 4;
   
 void setup() {
   FastLED.addLeds<WS2811, DATA_PIN>(leds, NUM_LEDS);
-  
 }
 
 void loop() {  
